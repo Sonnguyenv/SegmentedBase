7f0d97b0eb0440f4d2c0fc0d6742be19b6c94a6a
SonNV-D1 <sonnguyenv.mta@gmail.com>
2019-03-18 10:14:46 +0700

    create SegmentPager

---
+++ SegementPager/SegementPager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegementPager
+//
+//  Created by SonNV-D1 on 3/18/19.
+//  Copyright © 2019 SonNV-D1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
